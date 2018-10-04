7d706558a8ff859b31f8c949475ba38fb2112c80
robzz <robin94320@gmail.com>
2018-10-04 11:50:26 +0200

    Prepare first page

--- node/server.js
+++ node/server.js
@@ -2,27 +2,9 @@ const express = require('express');
 
 const app = express();
 
-function makeServer() {
-    app.use(express.static(__dirname + '/public'));
-    app.get('/', function(req, res) {
-        res.sendFile(__dirname + '/public/html/index.html');
-        });
+app.get('/', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.sendFile(__dirname + '/public/html/index.html');
+});
 
-    app.get('/authent.html', function(req, res) {
-        res.sendFile(__dirname + '/public/html/authent.html');
-        });
-
-        return app.listen(3000);
-    }
-
-
-const Serv = makeServer();
-
-function abc() {
-    return "abc";
-}
-
-module.exports = {
-    Serv: Serv,
-    abc: abc
-}
+app.listen(3000);
